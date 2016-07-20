bc91277c4f6d53663abbf1466dec6c02074ffa13
Brian Heim <bheim67@gmail.com>
2016-07-19 18:09:36 -0600

    pushing latest updates

--- lib/ship.rb
+++ lib/ship.rb
@@ -1,30 +1,39 @@
 require 'pry'
-require_relative 'communication'
+# require_relative 'communication'
 
 class Ship
-  attr_reader :length, :location
+  attr_reader :size, :location
 
-  def initialize(length, location)
-    @length = length
+  def initialize(size, location)
+    @size = size
     @location = location
   end
 
-  def valid_length?
-    if length == "2" || length == "3"
+  def valid_size?
+    if size == "2" || size == "3"
       true
     else
       false
     end
   end
 
-  def valid_location?
+  def valid_location_format?
+    if location.length == 2
+      true
+    else
+      false
+    end
+  end
 
+  def horizontal?
+    bow = ship.location[0]
+    stern = ship.location[1]
 
   end
 
 
-
-
-
-
 end
+
+new_ship = Ship.new("3", ["A1", "A3"])
+new_ship.horizontal?
+binding.pry
--- test/ship_test.rb
+++ test/ship_test.rb
@@ -9,9 +9,9 @@ require 'pry'
 
 class ShipTest < Minitest::Test
 
-  def test_it_has_a_length
+  def test_it_has_a_size
     ship = Ship.new("3", ["A1", "A3"])
-    assert_equal "3", ship.length
+    assert_equal "3", ship.size
   end
 
   def test_it_has_a_location
@@ -19,20 +19,41 @@ class ShipTest < Minitest::Test
     assert_equal ["A1", "A3"], ship.location
   end
 
-  def test_it_has_a_valid_length
+  def test_it_has_a_valid_size
     ship = Ship.new("3", ["A1", "A3"])
-    assert ship.valid_length?
+    assert ship.valid_size?
   end
 
-  def test_it_has_an_invalid_length
+  def test_it_has_an_invalid_size
     ship = Ship.new("4", ["A1", "A3"])
-    refute ship.valid_length?
+    refute ship.valid_size?
   end
 
-  def test_it_has_a_valid_location
-    
-
+  def test_it_has_a_valid_location_format
+    ship = Ship.new("3", ["A1", "A3"])
+    assert ship.valid_location_format?
   end
+
+  def test_it_has_an_invalid_location_format
+    ship = Ship.new("3", ["A1", "A2", "A3"])
+    refute ship.valid_location_format?
+  end
+
+  def test_it_is_horizo
+    skip
+    ship = Ship.new("3", ["A1", "A3"])
+    assert ship.horizontal?
+  end
+
+
+
+
+
+
+
+
+
+
 
 
 
